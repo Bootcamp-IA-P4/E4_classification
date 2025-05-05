511e2bd5bd6635d4f6b7fda76fa1935c1b725ca3
a-bac-0 <anca.bacria@thevallians.com>
2025-05-05 03:34:52 +0200

    feat: creating input example to check form before connecting to db

--- db/db.py
+++ db/db.py
@@ -1,13 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.models import Base
+from dotenv import load_dotenv
+import os
 
-DB_USER = "root"
-DB_PASSWORD = "password"
-DB_HOST = "localhost"
-DB_NAME = "registros_usuarios"
+load_dotenv()
+print(f"DB_USER: {os.getenv('MYSQL_USER')}")
+print(f"DB_PASSWORD: {os.getenv('MYSQL_PASSWORD')}")
+print(f"DB_HOST: {os.getenv('MYSQL_HOST')}")
+print(f"DB_NAME: {os.getenv('MYSQL_DB')}")
+print(f"DB_PORT: {os.getenv('MYSQL_PORT')}")
 
-DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DB_USER = os.getenv("MYSQL_USER")
+DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
+DB_HOST = os.getenv("MYSQL_HOST")
+DB_NAME = os.getenv("MYSQL_DB")
+DB_PORT = os.getenv("MYSQL_PORT")
+
+DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+print(f"DATABASE_URL: {DATABASE_URL}")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
