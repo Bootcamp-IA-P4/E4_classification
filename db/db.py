ac7c31fc48b4c71cb23af4418a574f3eb48678ba
a-bac-0 <anca.bacria@thevallians.com>
2025-05-05 03:34:52 +0200

    feat: creating input example to check form before connecting to db

--- db/db.py
+++ db/db.py
@@ -6,23 +6,21 @@ import os
 from urllib.parse import quote_plus
 
 load_dotenv()
+print(f"DB_USER: {os.getenv('MYSQL_USER')}")
+print(f"DB_PASSWORD: {os.getenv('MYSQL_PASSWORD')}")
+print(f"DB_HOST: {os.getenv('MYSQL_HOST')}")
+print(f"DB_NAME: {os.getenv('MYSQL_DB')}")
+print(f"DB_PORT: {os.getenv('MYSQL_PORT')}")
 
 DB_USER = os.getenv("MYSQL_USER")
-DB_PASSWORD_RAW = os.getenv("MYSQL_PASSWORD")
-DB_PASSWORD = quote_plus(DB_PASSWORD_RAW) if DB_PASSWORD_RAW else ''
+DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
 DB_HOST = os.getenv("MYSQL_HOST")
 DB_NAME = os.getenv("MYSQL_DB")
 DB_PORT = os.getenv("MYSQL_PORT")
 
-# Crear engine temporal para crear la base de datos si no existe
-tmp_engine = create_engine(f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/")
-try:
-    with tmp_engine.connect() as conn:
-        conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}"))
-        conn.commit()
-except Exception as e:
-    print(f"Error creando la base de datos: {e}")
+DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+print(f"DATABASE_URL: {DATABASE_URL}")
 DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 print(f"DATABASE_URL: {DATABASE_URL}")
